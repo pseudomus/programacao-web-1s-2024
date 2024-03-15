5cd6c16929f6a2e9dc13c422b166dea03c1cec78
Luca Lacerda <lucagabriel520@gmail.com>
2024-03-15 14:05:12 -0300

    Atividade01 adicionada

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,33 @@
+let soma1 = 5;
+let soma2 = 3;
+
+let sub1 = 10;
+let sub2 = 7;
+
+let div1 = 5;
+let div2 = 8;
+
+let mult1 = 5;
+let mult2 = 6;
+
+let resto1 = 8;
+let resto2 = 5;
+
+let exp1 = 2;
+let exp2 = 3;
+
+// Realização das operações e armazenamento dos resultados
+let soma = soma1 + soma2;
+let subtracao = sub1 - sub2;
+let multiplicacao = mult1 * mult2;
+let divisao = div1 / div2;
+let resto = resto1 % resto2;
+let exponencial = exp1 ** exp2;
+
+// Impressão dos resultados
+console.log(`Resultado da soma entre ${soma1} e ${soma2}: ${soma}`);
+console.log(`Resultado da subtração entre ${sub1} e ${sub2}: ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${mult1} e ${mult2}: ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${div1} e ${div2}: ${divisao}`);
+console.log(`Resultado do resto entre ${resto1} e ${resto2}: ${resto}`);
+console.log(`Resultado do exponencial entre ${exp1} e ${exp2}: ${exponencial}`);
